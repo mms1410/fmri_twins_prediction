4e052e0143a139ba7c223a9af8dfa3322f2b84ff
zentariko <>
2023-10-01 22:50:35 +0200

    flake8 errors resolved

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -1,32 +1,44 @@
 """
-
-saves the downloaded data under data / raw
+saves the downloaded fmri data of twins under data/raw.
 
 installation of datalad:
 https://docs.datalad.org/en/latest/generated/man/datalad-clone.html
-e.g. to install in ubuntu:
+e.g. installation in ubuntu via:
 sudo apt-get install datalad
 
 """
+import logging
 import os
 import shutil
+
 import datalad.api as dl
 
-dataset_accession_number = 'ds004169'
 
-dataset_url = f'https://github.com/OpenNeuroDatasets/{dataset_accession_number}.git'
+def download_dataset(dataset_accession_number: str = "ds004169"):
+    """Download dataset from open neuro datasets.
 
-dataset = dl.clone(source=dataset_url)
-dl.get(dataset.path)
+    Args:
+        dataset_accession_number (str): neuro number.
+    """
+    url_part1 = "https://github.com/OpenNeuroDatasets/"
+    urlpart2 = f"{dataset_accession_number}.git"
+    dataset_url = url_part1 + urlpart2
 
-destination_dir = os.path.join("data", "raw", dataset_accession_number)
+    dataset = dl.clone(source=dataset_url)
+    dl.get(dataset.path)
 
-if not os.path.exists(destination_dir):
-    os.makedirs(destination_dir)
+    destination_dir = os.path.join("data", "raw", dataset_accession_number)
 
-# Move the folder to the destination
-shutil.move(dataset_accession_number, destination_dir)
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
 
-# todo: logging
-print(
-    f"raw fmri data {dataset_accession_number} downloaded and saved to {destination_dir}")
+    # Move the folder to the destination:
+    shutil.move(dataset_accession_number, destination_dir)
+    logger = logging.getLogger(__name__)
+    logger.info(
+        f"raw fmri data {dataset_accession_number} downloaded and " f"saved to {destination_dir}"
+    )
+
+
+if __name__ == "__main__":
+    download_dataset()
