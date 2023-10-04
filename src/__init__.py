b5cdf6216c245718b83b3ff059e5c582597b23b0
zentariko <>
2023-10-04 16:25:00 +0200

    made src and test for downloding dataset compatbile with new tox file

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """An MLops project for predictions based on MRI data."""
 
-from .api import *  # noqa
+#from .api import *  # noqa
--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -14,30 +14,33 @@ import shutil
 import datalad.api as dl
 
 
-def download_dataset(dataset_accession_number: str = "ds004169"):
+def download_dataset(
+    dataset_access_number: str = "ds004169",
+    destination_dir: str | None = None,
+):
     """Download dataset from open neuro datasets.
 
     Args:
-        dataset_accession_number (str): neuro number.
+        dataset_access_number: neuro number (str, optional)
+        destination_dir : path where the folders are stored (str | None, optional)
     """
     url_part1 = "https://github.com/OpenNeuroDatasets/"
-    urlpart2 = f"{dataset_accession_number}.git"
-    dataset_url = url_part1 + urlpart2
+    url_part2 = f"{dataset_access_number}.git"
+    dataset_url = url_part1 + url_part2
 
     dataset = dl.clone(source=dataset_url)
     dl.get(dataset.path)
 
-    destination_dir = os.path.join("data", "raw", dataset_accession_number)
+    if destination_dir is None:
+        destination_dir = os.path.join("data", "raw", dataset_access_number)
 
     if not os.path.exists(destination_dir):
         os.makedirs(destination_dir)
 
     # Move the folder to the destination:
-    shutil.move(dataset_accession_number, destination_dir)
+    shutil.move(dataset_access_number, destination_dir)
     logger = logging.getLogger(__name__)
-    logger.info(
-        f"raw fmri data {dataset_accession_number} downloaded and " f"saved to {destination_dir}"
-    )
+    logger.info(f"raw fmri data {dataset_access_number} downloaded and saved to {destination_dir}")
 
 
 if __name__ == "__main__":
